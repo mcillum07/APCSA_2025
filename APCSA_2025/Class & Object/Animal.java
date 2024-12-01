1adc9b08088c54c2c1b1a1c0feca4b5087d9654c
MartinCao02 <77289960+mcillum07@users.noreply.github.com>
2024-11-30 21:17:39 -0800

    Week 9: Class & Object files uploaded

---
+++ APCSA_2025/Class & Object/Animal.java
@@ -0,0 +1,33 @@
+public class Animal {
+    
+    // Fields / Properties
+    // please add 4 different properties that an animal could have.
+    // In this 4, you want to include a variable that will indicate what kind of animal it is.
+    // Beside the 4 you decide, please add a double value named: runDistance
+
+
+
+
+    // Constructor
+    // Please complete your constructor build
+
+
+
+    // Methods
+    // Please add any getter and setter if you think it's needed.
+
+
+    // Complete the following Method:
+    /* This method will compare the runDistance between two animals (this and other) 
+     * Think about how to get the information you need.
+    */
+
+    // public String whoRunsMore(Animal other) {
+
+    // }
+
+    public static void main(String[] args) {
+        // create two Animal obj
+        
+    }
+}
---
+++ APCSA_2025/Class & Object/Class9.java
@@ -0,0 +1,9 @@
+public class SatClass9 {
+
+    public static void main(String[] args) {
+        // new must be used when you declare an non-String object.
+        Example exObj = new Example();
+        System.out.println(exObj);
+
+    }
+}
